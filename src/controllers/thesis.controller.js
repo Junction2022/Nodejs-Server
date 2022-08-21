92c44bb8c6059e0f4586f923a8d1df3f1b9de9a4
whitebear05 <woojs0505@naver.com>
2022-08-21 11:07:22 +0900

    :memo: :: add TODO

--- src/controllers/thesis.controller.js
+++ src/controllers/thesis.controller.js
@@ -3,16 +3,19 @@ const catchAsync = require('../utils/catchAsync');
 const thesisService = require('../services/thesis.service');
 
 const getAll = catchAsync(async (req, res) => {
+  // TODO : Connect with BlockChain Server
   const thesisList = await thesisService.getAllThesis();
   res.status(httpStatus.OK).send({ thesisList });
 });
 
 const getById = catchAsync(async (req, res) => {
+  // TODO : Connect with BlockChain Server
   const thesis = await thesisService.getThesisById(req.params.id);
   res.status(httpStatus.OK).send({ thesis });
 });
 
 const create = catchAsync(async (req, res) => {
+  // TODO : Connect with BlockChain Server
   const { title, file, language, topic, pageCount, author } = req.body;
   const thesis = await thesisService.createThesis(title, file, language, topic, pageCount, author);
   res.status(httpStatus.CREATED).send({ thesis });
